4435e96c0a0ac8380f649b071252f41ce7dba5b1
Queen-Orion <leobrer77@gmail.com>
2022-06-10 15:48:51 -0700

    Squared Simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 computes the square value of all integers of a matrix.
-Args: 
+
+Args:
     matrix: 2D list to perform operation on
 Return:
     same size and square of all value of a matrix
--- 0x04-python-more_data_structures/README.md
+++ 0x04-python-more_data_structures/README.md
@@ -1,1 +1,1 @@
-More Python data structures
+More python data structures
