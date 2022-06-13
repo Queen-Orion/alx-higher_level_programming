6387fb6676246885c480c910e1384733e71ddf59
Queen-Orion <leobrer77@gmail.com>
2022-06-12 17:29:43 -0700

    Multiply by 2

--- 0x04-python-more_data_structures/7-main.py
+++ 0x04-python-more_data_structures/7-main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-update_dictionary = __import__('7-update_dictionary').update_dictionary
+update_dictionary = \
+        __import__('7-update_dictionary').update_dictionary
 print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_
 dictionary
 
