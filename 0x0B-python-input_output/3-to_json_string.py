102560cad25ed074689e72b524d29401e3d22194
Queen-Orion <leobrer77@gmail.com>
2022-07-08 05:19:07 -0700

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/python3
+"""to_json sting module"""
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object (string)"""
+    return json.dumps(my_obj)
